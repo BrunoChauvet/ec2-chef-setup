17f1a3a339563a028b70760d8a02abba2733fece
Bruno Chauvet <bruno.chauvet@live.fr>
2014-01-20 17:37:05 +1100

    Renamed scripts and git repo

--- initialise.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo yum update
-sudo yum install git
-git clone git://github.com/BrunoChauvet/myrepo.git
-sh myrepo/install.sh
---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install chef-solo
+curl -L https://www.opscode.com/chef/install.sh | sudo bash
+# Install AWS tools
+sudo yum install python-pip
+sudo pip install --upgrade awscli
+# ... run chef
+# - mysql5
+# - java
+# - tomcat
+# - ...
